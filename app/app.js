67ecd607cfa91c550a466252518030bc2e297502
Max Strübing <max_struebing@gmx.de>
2015-12-18 21:32:02 +0100

    Add request and cheerio to dependencies and add index.js and app.js with usage

---
+++ app/app.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+function printUsage() {
+  console.log('imdb-node ARGUMENT');
+  console.log('WHERE');
+  console.log('  Argument is the imdb-id or link');
+}
+
+printUsage();
